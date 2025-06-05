f91d414ca113f7ad4bd059f69a4a636790101ba0
ianneyy <jimmygomez1230@gmail.com>
2025-06-05 21:36:44 +0800

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@
 import { Outlet } from "react-router-dom";
 import { ThemeProvider } from "@/theme-provider";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Outlet />
+      <Analytics />
+      
     </ThemeProvider>
   );
 }
